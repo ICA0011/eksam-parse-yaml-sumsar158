5a844e2c1ecf3c80767aafcdfaef38abccdff249
rasmu <rasmus.pidim@gmail.com>
2022-06-06 13:43:20 +0300

    1

--- parse_yaml.py
+++ parse_yaml.py
@@ -1,9 +1,18 @@
-import yaml, requests
+import yaml
+import requests
 
 url = 'http://upload.itcollege.ee/~aleksei/eksam.yaml'
 
+
 def parse_yaml(url):
-    
-    # your code here
-    
-    return name_of_the_user
+
+    for user in yaml.load(requests.get(url).text, yaml.FullLoader):
+        print(user)
+        for k, v in user.items():
+
+            if v["permission"] == "admin":
+                return k
+
+
+if __name__ == '__main__':
+    parse_yaml(url)
--- parse_yaml_test.py
+++ parse_yaml_test.py
@@ -1,4 +1,5 @@
 import parse_yaml
 
+
 def test_assignment():
     assert parse_yaml.parse_yaml('http://upload.itcollege.ee/~aleksei/eksam.yaml') == "Toomas Teine"
