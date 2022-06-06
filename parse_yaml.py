df1b67b1900bd47ab068b518f7d43c5ee8872967
rasmu <rasmus.pidim@gmail.com>
2022-06-06 13:52:02 +0300

    3

--- parse_yaml.py
+++ parse_yaml.py
@@ -7,12 +7,9 @@ url = 'http://upload.itcollege.ee/~aleksei/eksam.yaml'
 def parse_yaml(url):
 
     for user in yaml.load(requests.get(url).text, yaml.FullLoader):
-        print(user)
         for k, v in user.items():
 
             if v["permission"] == "admin":
                 return v["name"]
 
 
-if __name__ == '__main__':
-    print(parse_yaml(url))
